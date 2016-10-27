a48148a5e097aafe53cd3c119d68c272d70a46a4
tdhuynh <tommyhuynh93@gmail.com>
2016-10-27 17:57:25 -0400

    still working on math

--- iron_bank/iron_bank/settings.py
+++ iron_bank/iron_bank/settings.py
@@ -122,7 +122,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
-
+LOGIN_REDIRECT_URL = "/"
 # REST_FRAMEWORK = {
 #     'DEFAULT_AUTHENTICATION_CLASSES': (
 #         'rest_framework.authentication.TokenAuthentication',
--- iron_bank/iron_bank_api/views.py
+++ iron_bank/iron_bank_api/views.py
@@ -10,12 +10,23 @@ class IndexCreateView(CreateView):
     success_url = reverse_lazy("index_create_view")
     fields = ('amount', 'process_type')
 
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["transaction_list"] = Transaction.objects.filter(account=self.request.user)
+        return context
+
     def get_queryset(self):
         return Transaction.objects.filter(account=self.request.user)
 
     def balance(self):
         all_transactions = Transaction.objects.filter(account=self.request.user)
-        
+        for transaction in all_transactions:
+            if transaction.process_type == "W":
+                withdraw = transaction.amount * -1
+            else:
+                deposit = transaction.amount
+            return sum(deposit) + sum(withdraw)
 
 
 
